14cb129767d1eba6079f51b0ac16198a57da422c
Brian Monge <bm0801389031@gmail.com>
2021-03-27 00:25:21 -0700

    complete file seats in theater

---
+++ seatsInTheater/final/logic.js
@@ -0,0 +1,7 @@
+//function statement named seatsInTheater with four parameters
+// nCols, nRows, col, row.
+function seatsInTheater(nCols, nRows, col, row) {
+    //return nCols minus col plus 1 times nRows minus rows
+    //follow the order of operations
+        return (nCols-col+1) * (nRows - row);
+    }
---
+++ seatsInTheater/setup/logic.js
@@ -0,0 +1,5 @@
+//function statement named seatsInTheater with four parameters
+// nCols, nRows, col, row.
+function seatsInTheater(nCols, nRows, col, row) {
+
+    }
